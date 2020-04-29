6d8948072f102bc9183abc78ab1058d5aecf1e02
JASSEN_WU <jassen.wu@oracle.com>
2020-04-29 12:54:21 +0800

    index

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ set -e -x
 docker build --force-rm -t sora:latest --file docker/Dockerfile .
 docker tag sora:latest jassen75/sora:1.0
 
-docker login -u jassen75 --password=j20161017 
+docker login -u jassen75 --password=$PASSWORD 
 docker push jassen75/sora:1.0
